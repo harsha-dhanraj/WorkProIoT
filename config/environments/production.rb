045ae4eb5fc9d5dfb267bfa1b278b176043102ee
Harshvardhan <harshu.rtk03@gmail.com>
2017-12-18 04:18:00 +0530

    Production smtp settings added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,16 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
+
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+      :address              => 'smtp.gmail.com',
+      :port                 => 587,
+      :domain               => 'gmail.com',
+      :user_name            => ENV["GMAIL_USERNAME"],
+      :password             => ENV["GMAIL_PASSWORD"],
+      :authentication       => 'login',
+      :enable_starttls_auto => true
+ }
+
 end
